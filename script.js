10cb7fbede2187d4918bf48a4f655776e19a3ca0
CleberRoberto <107493696+cleberrobertodg@users.noreply.github.com>
2023-02-09 16:33:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "black";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16* box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++) {
-        context.fillStyle = "white";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
@@ -48,6 +48,14 @@ function drawFood(){
         if(snake[0].x <0 && direction == "left") snake[0].x = 16 * box;
         if(snake[0].y >15 * box && direction == "down") snake[0].y = 0;
         if(snake[0].y <0 && direction == "up") snake[0].y = 16 * box;
+
+        for(i = 1; i < snake.length; i++){
+            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+                clearInterval(jogo);
+                alert('Perdeu Playboy, de Ok, e após isso F5 para iniciar novo jogo')
+            }
+        }
+
         criarBG();
         criarCobrinha();
         drawFood();
